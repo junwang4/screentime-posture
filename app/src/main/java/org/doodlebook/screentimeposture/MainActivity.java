b5aa9bef64c92742577372ae7863306fd013df86
jwang72 <junwang4@gmail.com>
2017-03-14 21:19:11 -0400

    finally - it works (both service and sensor listener) SOLUTION: put sensorEventListener in MainActivity

--- app/src/main/java/org/doodlebook/screentimeposture/MainActivity.java
+++ app/src/main/java/org/doodlebook/screentimeposture/MainActivity.java
@@ -13,6 +13,7 @@ import android.view.View;
 
 import android.content.Intent;
 import android.widget.TextView;
+import android.util.Log;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -32,12 +33,33 @@ public class MainActivity extends AppCompatActivity {
     static TextView vStatus;
     MyService myService = new MyService();
 
+    static UsageStatsManager usageStatsManager;
+    static SensorManager sensorManager;
+    static Sensor sensorAccelerometer, sensorMagnetic, sensorGyro;
+
+    static HashMap<Integer, Long> sensorPrevmillis = new HashMap<Integer, Long>();
+    static HashMap<Integer, String> sensorTypeName = new HashMap<Integer, String>();
+    static boolean isSensorRegistered;
+    static StringBuilder data;
+    static long listenCount = 0;
+    static long serviceRunCount = 0;
+
+    static RequestQueue requestQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(org.doodlebook.screentimeposture.R.layout.activity_main);
         vStatus = (TextView) findViewById(org.doodlebook.screentimeposture.R.id.textViewStatus);
+
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        sensorAccelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        sensorMagnetic = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+        sensorGyro = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+
+        usageStatsManager = (UsageStatsManager) getSystemService(Context.USAGE_STATS_SERVICE);
+
+        requestQueue = Volley.newRequestQueue(getApplicationContext());
     }
 
     public void startMyService(View view) {
@@ -48,78 +70,35 @@ public class MainActivity extends AppCompatActivity {
         myService.cancelRepeat(view.getContext());
     }
 
+    public static void unRegisterSensorListener() {
+        sensorManager.unregisterListener(sensorEventListener);
+    }
+    public static void registerSensorListener() {
+        sensorManager.registerListener(sensorEventListener, sensorAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+        MainActivity.vStatus.setText("registerSensorListener " + MyUtil.formatter.format(new Date()));
 
+        long prev_millis = System.currentTimeMillis();
+        sensorPrevmillis.put(Sensor.TYPE_ACCELEROMETER, prev_millis);
+        sensorPrevmillis.put(Sensor.TYPE_GYROSCOPE, prev_millis);
+        sensorPrevmillis.put(Sensor.TYPE_MAGNETIC_FIELD, prev_millis);
+        sensorTypeName.put(Sensor.TYPE_ACCELEROMETER, "ACC");
+        sensorTypeName.put(Sensor.TYPE_GYROSCOPE, "GYRO");
+        sensorTypeName.put(Sensor.TYPE_MAGNETIC_FIELD, "MAGNET");
 
-    private String getAppUsageStatsTest() {
-        UsageStatsManager lUsageStatsManager = (UsageStatsManager) getSystemService(Context.USAGE_STATS_SERVICE);
-        List<UsageStats> lUsageStatsList =
-                lUsageStatsManager.queryUsageStats(UsageStatsManager.INTERVAL_DAILY,
-                        System.currentTimeMillis() - TimeUnit.MINUTES.toMillis(1),
-                        System.currentTimeMillis() + TimeUnit.MINUTES.toMillis(1));
-
-        StringBuilder lStringBuilder = new StringBuilder();
-
-        long currMillis = System.currentTimeMillis();
-        for (UsageStats lUsageStats : lUsageStatsList) {
-            long lastTimeMillis = lUsageStats.getLastTimeUsed();
-            if (currMillis-lastTimeMillis < 120*1000) {
-                lStringBuilder.append(lUsageStats.getPackageName());
-                lStringBuilder.append("|");
-                lStringBuilder.append(MyUtil.millisToTimeString(lastTimeMillis));
-                lStringBuilder.append("|");
-                lStringBuilder.append(lUsageStats.getTotalTimeInForeground() / 1000);
-                lStringBuilder.append("\n");
-            }
-        }
-        return lStringBuilder.toString();
+        data = new StringBuilder();
+        listenCount = 0;
+        serviceRunCount = 0;
+        isSensorRegistered = true;
     }
 
 
-}
-
-    /*
-    void getAppUsageAndSensorData() {
-        if (! isSensorRegistered) {
-            isSensorRegistered = true;
-
-//            sensorManager.registerListener(sensorEventListener, sensorAccelerometer, 20000000);
-            //sensorManager.registerListener(sensorEventListener, sensorMagnetic, 20000000);
-            //sensorManager.registerListener(sensorEventListener, sensorGyro, 20000000);
-            sensorManager.registerListener(this, sensorAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
-
-            long prev_millis = System.currentTimeMillis();
-            sensorPrevmillis.put(Sensor.TYPE_ACCELEROMETER, prev_millis);
-            sensorPrevmillis.put(Sensor.TYPE_GYROSCOPE, prev_millis);
-            sensorPrevmillis.put(Sensor.TYPE_MAGNETIC_FIELD, prev_millis);
-
-            sensorTypeName.put(Sensor.TYPE_ACCELEROMETER, "ACC");
-            sensorTypeName.put(Sensor.TYPE_GYROSCOPE, "GYRO");
-            sensorTypeName.put(Sensor.TYPE_MAGNETIC_FIELD, "MAGNET");
-
-            data = new StringBuilder();
-            MainActivity.vStatus.setText(MyUtil.formatter.format(new Date()) + "register listener");
-        } else {
-            isSensorRegistered = false;
-            sensorManager.unregisterListener(this);
-            MainActivity.vStatus.setText(MyUtil.formatter.format(new Date()) + "suppose to unregister listener");
-            saveData();
-            data = new StringBuilder();
-        }
-    }
-*/
-
-        /*
-    SensorEventListener sensorEventListener = new SensorEventListener() {
+    public static SensorEventListener sensorEventListener = new SensorEventListener() {
         public void onAccuracyChanged(Sensor sensor, int accuracy) {}
-        public void onSensorChanged(SensorEvent event) {
-            if (!isSensorRegistered) {
-                sensorManager.unregisterListener(sensorEventListener); // in case unregisterListener not works
-                return;
-            }
 
+        public void onSensorChanged(SensorEvent event) {
             int sensorType = event.sensor.getType();  // 1: accelerator, 2: magnetic, 4: gyroscope, ??: light
             long curr_millis = System.currentTimeMillis();
-            if (curr_millis - sensorPrevmillis.get(sensorType) < 5000)
+            if (curr_millis - sensorPrevmillis.get(sensorType) < 2000)
                 return;
             sensorPrevmillis.put(sensorType, curr_millis);
 
@@ -129,9 +108,87 @@ public class MainActivity extends AppCompatActivity {
                     + "," + sensorType + "," + sensorTypeName.get(sensorType)
                     + "," + xyz[0] + "," + xyz[1] + "," + xyz[2] + "\n";
 
-            MainActivity.vStatus.setText(record);
+            MainActivity.vStatus.setText(++listenCount + "   \n" + record);
             data.append(record);
+            if (listenCount % 5 == 0)
+                saveData();
         }
     };
-*/
 
+
+    static void saveData() {
+        sendDataToWebServer("service", "posture_xyz", data.toString(),
+            new MyUtil.VolleyCallback() {
+                @Override
+                public void onSuccess(String result){
+                    MainActivity.vStatus.setText(result);
+                }
+                public void onError(String result){
+                    MainActivity.vStatus.setText("Error: " + result);
+                }
+            });
+    }
+
+    static void sendDataToWebServer(String task, String data_type, String data, final MyUtil.VolleyCallback callback) {
+        final String data_type1 = data_type;
+        final String data1 = data;
+        final String task1 = task;
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, MyUtil.SERVER_URL,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        callback.onSuccess(response);
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        callback.onError(error.toString());
+                        Log.e("Error", error.toString());
+                    }
+                })
+        {
+            @Override
+            protected Map<String,String> getParams(){
+                Map<String,String> params = new HashMap<String, String>();
+                params.put("task", task1);
+                params.put("type", data_type1);
+                params.put("data", data1);
+                return params;
+            }
+        };
+
+        //RequestQueue requestQueue = Volley.newRequestQueue(this); // see onCreate() : Volley.newRequestQueue(getApplicationContext())
+        requestQueue.add(stringRequest);
+    }
+
+
+
+
+    public void clickSituationBed(View view) {
+        MainActivity.vStatus.setText("test...");
+
+        UsageStatsManager lUsageStatsManager = (UsageStatsManager) getSystemService(Context.USAGE_STATS_SERVICE);
+        List<UsageStats> lUsageStatsList =
+                lUsageStatsManager.queryUsageStats(UsageStatsManager.INTERVAL_DAILY,
+                        System.currentTimeMillis() - TimeUnit.MINUTES.toMillis(60),
+                        System.currentTimeMillis() + TimeUnit.MINUTES.toMillis(60));
+
+        StringBuilder lStringBuilder = new StringBuilder();
+
+        long currMillis = System.currentTimeMillis();
+        for (UsageStats lUsageStats : lUsageStatsList) {
+            long lastTimeMillis = lUsageStats.getLastTimeUsed();
+            if (currMillis-lastTimeMillis < 2*1000) {
+            lStringBuilder.append(lUsageStats.getPackageName());
+            lStringBuilder.append("|");
+            lStringBuilder.append(MyUtil.millisToTimeString(lastTimeMillis));
+            lStringBuilder.append("|");
+            lStringBuilder.append(lUsageStats.getTotalTimeInForeground() / 1000);
+            lStringBuilder.append("\n");
+            }
+        }
+        MainActivity.vStatus.setText(lStringBuilder.toString());
+    }
+
+}
--- app/src/main/java/org/doodlebook/screentimeposture/MyService.java
+++ app/src/main/java/org/doodlebook/screentimeposture/MyService.java
@@ -31,45 +31,10 @@ import java.util.List;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
-public class MyService extends GcmTaskService implements SensorEventListener {
+public class MyService extends GcmTaskService {
 
     private static final String TAG = MyService.class.getSimpleName();
     public static final String GCM_REPEAT_TAG = "repeat|[7200,1800]";
-
-    static SensorManager sensorManager;
-    static Sensor sensorAccelerometer, sensorMagnetic, sensorGyro;
-    static boolean isSensorRegistered;
-    static StringBuilder data;
-    static long listenCount = 0;
-    static long serviceRunCount = 0;
-
-
-    public void onAccuracyChanged(Sensor sensor, int accuracy) {}
-    public void onSensorChanged(SensorEvent event) {
-        if (!isSensorRegistered) {
-            return;
-        }
-
-        int sensorType = event.sensor.getType();  // 1: accelerator, 2: magnetic, 4: gyroscope, ??: light
-        long curr_millis = System.currentTimeMillis();
-        if (curr_millis - sensorPrevmillis.get(sensorType) < 2000)
-            return;
-        sensorPrevmillis.put(sensorType, curr_millis);
-
-        float[] xyz = event.values;
-        String record = MyUtil.posture_choice + "," + MyUtil.PHONE_INFO
-                + "," + curr_millis + "," + MyUtil.formatter.format(new Date())
-                + "," + sensorType + "," + sensorTypeName.get(sensorType)
-                + "," + xyz[0] + "," + xyz[1] + "," + xyz[2] + "\n";
-
-        MainActivity.vStatus.setText(++listenCount + "   \n" + record);
-        data.append(record);
-
-        if (listenCount % 10 == 0) {
-            saveData();
-            data = new StringBuilder();
-        }
-    }
 
 
     @Override
@@ -77,36 +42,11 @@ public class MyService extends GcmTaskService implements SensorEventListener {
         // called when app is updated to a new version, reinstalled etc.
         // you have to schedule your repeating tasks again
         super.onInitializeTasks();
-        myStartSensorListener();
-    }
-
-
-    void myStartSensorListener() {
-        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        sensorAccelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        sensorMagnetic = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
-        sensorGyro = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
-
-        sensorManager.registerListener(this, sensorAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
-        MainActivity.vStatus.setText("myStart " + MyUtil.formatter.format(new Date()) + "   register listener  " + listenCount);
-
-        long prev_millis = System.currentTimeMillis();
-        sensorPrevmillis.put(Sensor.TYPE_ACCELEROMETER, prev_millis);
-        sensorPrevmillis.put(Sensor.TYPE_GYROSCOPE, prev_millis);
-        sensorPrevmillis.put(Sensor.TYPE_MAGNETIC_FIELD, prev_millis);
-        sensorTypeName.put(Sensor.TYPE_ACCELEROMETER, "ACC");
-        sensorTypeName.put(Sensor.TYPE_GYROSCOPE, "GYRO");
-        sensorTypeName.put(Sensor.TYPE_MAGNETIC_FIELD, "MAGNET");
-
-        data = new StringBuilder();
-        listenCount = 0;
-        serviceRunCount = 0;
-        isSensorRegistered = true;
     }
 
 
     public void scheduleRepeat(Context context) {
-        //in this method, single Repeating task is scheduled (the target service that will be called is MyTaskService.class)
+
         try {
 
             PeriodicTask periodic = new PeriodicTask.Builder()
@@ -147,17 +87,12 @@ public class MyService extends GcmTaskService implements SensorEventListener {
             h.post(new Runnable() {
                 @Override
                 public void run() {
+                    Toast.makeText(MyService.this, "onRunTask executed: ", Toast.LENGTH_LONG).show();
+
+                    getAppUsageStats();
+
                     //String data = myUtil.scanWifi("train");
 
-                    //getAppUsageAndSensorData();
-                    isSensorRegistered = !isSensorRegistered;
-                    /*if (!isSensorRegistered) {
-                        saveData();
-                        data = new StringBuilder();
-                    }*/
-
-
-                    Toast.makeText(MyService.this, "onRunTask executed: " + isSensorRegistered, Toast.LENGTH_LONG).show();
                 }
             });
         }
@@ -166,66 +101,46 @@ public class MyService extends GcmTaskService implements SensorEventListener {
 
 
     public void cancelRepeat(Context context) {
-        sensorManager.unregisterListener(this);
-        isSensorRegistered = false;
-        MainActivity.vStatus.setText(MyUtil.formatter.format(new Date()) + " : suppose to unregister listener");
-
         Toast.makeText(context, "Stop the background service", Toast.LENGTH_LONG).show();
-
+        MainActivity.unRegisterSensorListener();
         GcmNetworkManager.getInstance(context).cancelTask(GCM_REPEAT_TAG, MyService.class);
     }
 
 
-    HashMap<Integer, Long> sensorPrevmillis = new HashMap<Integer, Long>();
-    HashMap<Integer, String> sensorTypeName = new HashMap<Integer, String>();
+    private String getAppUsageStats() {
+        List<UsageStats> usageStatsList =
+                MainActivity.usageStatsManager.queryUsageStats(UsageStatsManager.INTERVAL_DAILY,
+                        System.currentTimeMillis() - TimeUnit.MINUTES.toMillis(5),
+                        System.currentTimeMillis() + TimeUnit.MINUTES.toMillis(5));
 
+        StringBuilder stringBuilder = new StringBuilder();
 
-    void saveData() {
-        sendDataToWebServer_temp("service", "posture_xyz", data.toString(),
-            new MyUtil.VolleyCallback() {
-                @Override
-                public void onSuccess(String result){
-                    MainActivity.vStatus.setText(result);
-                }
-                public void onError(String result){
-                    MainActivity.vStatus.setText("Error: " + result);
-                }
-            });
-    }
+        long currMillis = System.currentTimeMillis();
+        for (UsageStats usageStats : usageStatsList) {
+            long lastTimeMillis = usageStats.getLastTimeUsed();
+            if (currMillis-lastTimeMillis < 20*1000) {
+                stringBuilder.append(usageStats.getPackageName());
+                stringBuilder.append("|");
+                stringBuilder.append(MyUtil.millisToTimeString(lastTimeMillis));
+                stringBuilder.append("|");
+                stringBuilder.append(usageStats.getTotalTimeInForeground() / 1000);
+                stringBuilder.append("\n");
 
-    void sendDataToWebServer_temp(String task, String data_type, String data, final MyUtil.VolleyCallback callback) {
-        final String data_type1 = data_type;
-        final String data1 = data;
-        final String task1 = task;
-        StringRequest stringRequest = new StringRequest(Request.Method.POST, MyUtil.SERVER_URL,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        callback.onSuccess(response);
-                    }
-                },
-                new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        //Toast.makeText(thisActivity, error.toString(), Toast.LENGTH_LONG).show();
-                        callback.onError(error.toString());
-                    }
-                })
-        {
-            @Override
-            protected Map<String,String> getParams(){
-                Map<String,String> params = new HashMap<String, String>();
-                params.put("task", task1);
-                params.put("type", data_type1);
-                params.put("data", data1);
-                return params;
+                //if ( usageStats.getPackageName() == "com.android.chrome") myStartSensorListener();
             }
-        };
+        }
+        String usage = stringBuilder.toString();
+        if (usage.length()>0) {
+            Toast.makeText(MyService.this, usage, Toast.LENGTH_LONG).show();
+            MainActivity.registerSensorListener();
+        }
+        else {
+            MainActivity.unRegisterSensorListener();
+            Toast.makeText(MyService.this,  "unregister listener", Toast.LENGTH_LONG).show();
+        }
 
-        RequestQueue requestQueue = Volley.newRequestQueue(this);
-        requestQueue.add(stringRequest);
+        return stringBuilder.toString();
     }
-
 
 
 }
